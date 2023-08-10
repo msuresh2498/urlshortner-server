ef4de0d8a306a70f564e3fbee0da27f832989e4e
sureshm <msuresh2498@gmail.com>
2023-08-10 17:55:12 +0530

    error

--- Routes/url.js
+++ Routes/url.js
@@ -3,7 +3,7 @@ import { shortner, shorturl } from '../controllers/url.js';
 import { getUser, verifyToken } from '../controllers/user.js';
 
 const router = express.Router();
-
+// url shortner Routes
 router.post('/', verifyToken, getUser, shortner);
 router.get('/:shortid', shorturl);
 
