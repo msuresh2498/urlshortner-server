5f56ee96fe7114c2a17313dba1e27295b5cac072
sureshm <msuresh2498@gmail.com>
2023-08-16 11:49:01 +0530

    correction

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import database from '../urlshortner-server/db/connect.js'
 import * as dotenv from 'dotenv';
-import { MongoClient } from 'mongodb'
-import cors from 'cors'
-import userRoutes from '../urlshortner-server/Routes/user.js'
-import urlRoutes from '../urlshortner-server/Routes/url.js'
+import { MongoClient } from 'mongodb';
+import cors from 'cors';
+import userRoutes from '../urlshortner-server/Routes/user.js';
+import urlRoutes from '../urlshortner-server/Routes/url.js';
 import { User } from './models/usermodule.js';
 
 dotenv.config()
@@ -20,7 +20,7 @@ app.use(cors());
 database();
 
 app.use('/user', userRoutes);
-app.use('/url', urlRoutes)
+app.use('/url', urlRoutes);
 app.get('/', function (req, res) {
     res.send('Welcome to URL shortner APP')
 })
@@ -31,19 +31,16 @@ app.get('/', cors(), (req, res) => {
 })
 
 app.get('/urlList', async (req, res) => {
-
     try {
-        const list = await User.find({}, 'urlList'); 
-        if (list) {
-            return res.status(200).json({ urlList: list });
+        const userList = await User.find().select('urlList');
+        if (userList) {
+            return res.status(200).json({ urlList: userList });
         } else {
             return res.status(404).json({ message: 'No urlList found' });
         }
     } catch (error) {
         return res.status(500).json({ message: 'Error retrieving urlList', error: error.message });
     }
-})
-
-
+});
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
